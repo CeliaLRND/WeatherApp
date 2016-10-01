2291574e6cb6dffefa6fcec61736211c8bd03267
MAC <mac@IrisdeMacBook-Pro.local>
2016-10-01 10:51:30 +0800

    first commit

---
+++ WeatherApp/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by MAC on 29/09/2016.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by MAC on 30/09/2016.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+import Foundation
